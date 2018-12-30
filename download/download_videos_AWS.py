d749229b2ad8e980e44e138eac27fcdde1da5aab
syzhang <hefeizsy@163.com>
2018-12-30 00:01:22 -0500

    update

--- download/download_videos_AWS.py
+++ download/download_videos_AWS.py
@@ -1,22 +1,28 @@
-''' 
-Code to get the data in the DiDeMo video dataset using the videos stored on AWS.
-
-Usage:
-
-python download_videos_AWS.py  --download --video_directory DIRECTORY
-
-will download videos from flickr to DIRECTORY
-
-python download_videos_AWS.py  
 '''
+    Code to get the data in the DiDeMo video dataset using the videos stored on AWS.
+    
+    Usage:
+    
+    python download_videos_AWS.py  --download --video_directory DIRECTORY
+    
+    will download videos from flickr to DIRECTORY
+    
+    python download_videos_AWS.py
+    '''
+
 import sys
 sys.path.append('.')
-from utils.utils import *
 import urllib
 import urllib2
 import pdb
 import argparse
 import os
+import json
+
+def read_json(json_file):
+    with open(json_file) as data_file:
+        data = json.load(data_file)
+    return data
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_directory", type=str, default='videos/', help="Indicate where you want downloaded videos to be stored")
@@ -30,45 +36,45 @@ multimedia_template = 'https://multimedia-commons.s3-us-west-2.amazonaws.com/dat
 
 splits = ['test', 'val', 'train']
 data_template = 'data/%s_data.json'
-caps = [] 
+caps = []
 for split in splits:
-     caps.extend(read_json(data_template %split))
+    caps.extend(read_json(data_template %split))
 videos = set([cap['video'] for cap in caps])
- 
+
 def read_hash(hash_file):
     lines = open(hash_file).readlines()
     yfcc100m_hash = {}
     for line_count, line in enumerate(lines):
-         sys.stdout.write('\r%d/%d' %(line_count, len(lines)))
-         line = line.strip().split('\t')
-         yfcc100m_hash[line[0]] = line[1]
+        sys.stdout.write('\r%d/%d' %(line_count, len(lines)))
+        line = line.strip().split('\t')
+        yfcc100m_hash[line[0]] = line[1]
     print "\n"
     return yfcc100m_hash
 
 def get_aws_link(h):
-     return multimedia_template %(h[:3], h[3:6], h) 
+    return multimedia_template %(h[:3], h[3:6], h)
 
 yfcc100m_hash = read_hash('data/yfcc100m_hash.txt')
 
 missing_videos = []
 
 for video_count, video in enumerate(videos):
-     sys.stdout.write('\rDownloading video: %d/%d' %(video_count, len(videos)))
-     video_id = video.split('_')[1]
-     link = get_aws_link(yfcc100m_hash[video_id])
-     import pdb; pdb.set_trace()
-     if args.download:
+    sys.stdout.write('\nDownloading video: %d/%d' %(video_count, len(videos)))
+    video_id = video.split('_')[1]
+    link = get_aws_link(yfcc100m_hash[video_id])
+    # import pdb; pdb.set_trace()
+    if args.download:
         try:
             response = urllib2.urlopen(link)
             urllib.urlretrieve(response.geturl(), '%s/%s.mp4' %(args.video_directory, video))
         except:
             print "Could not download link: %s\n" %link
-     else:
-         try:
-             response = urllib2.urlopen(link)
-         except:
-             missing_videos.append(video)
-             print "Could not find link: %s\n" %link
+                else:
+                    try:
+                        response = urllib2.urlopen(link)
+                            except:
+                                missing_videos.append(video)
+                                print "Could not find link: %s\n" %link
 
 if len(missing_videos) > 0:
     write_txt = open('missing_videos.txt', 'w')
---
+++ generate_vocabulary.py
@@ -0,0 +1,29 @@
+import re
+import json
+
+def word_tokenize(s):
+    sent = s.lower()
+    sent = re.sub('[^A-Za-z0-9\s]+',' ', sent)
+    return sent.split()
+
+if __name__ == '__main__':
+
+    glove_dim = 300
+    glove_path = '/localdisk/szhang83/Developer/LocalizingMoments/data/glove.6B.%dd.txt' % glove_dim
+    glove_txt = open(glove_path).readlines()
+    glove_txt = [g.strip() for g in glove_txt]
+    glove_vector = [g.split(' ') for g in glove_txt]
+    glove_words = [g[0] for g in glove_vector]
+
+    txt_path = 'data/didemo_vocab_glove_complete.txt'
+    vocab_file = open(txt_path, 'w')
+    words = []
+    for split in ['train', 'val', 'test']:
+        anno_list = json.load(open('data/{}_data.json'.format(split)))
+
+        for anno in anno_list:
+            words.extend(word_tokenize(anno['description']))
+    valid_words = list(filter(lambda w: w in glove_words, set(words)))
+    vocab_file.write('\n'.join(set(valid_words)))
+
+
